0ed7bccfb5b74abc3b708b9d5b432e1c4e0e22b4
Himath_R <himath4510@gmail.com>
2022-10-28 23:22:43 +1000

    added es build & unpkg plugins

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,50 @@
+import { useState, useEffect, useRef } from "react";
 import ReactDOM from "react-dom";
+import * as esbuild from "esbuild-wasm";
 
 const App = () => {
-  return <div>Hi there!</div>;
+  const [input, setInput] = useState("");
+  const [code, setCode] = useState("");
+  const ref = useRef<any>();
+
+  const startService = async () => {
+    // we can refer to ref.current anywhere in the component, and we can use that to do our transpiling and bundling
+    ref.current = await esbuild.startService({
+      worker: true,
+      wasmURL: "/esbuild.wasm",
+    });
+  };
+
+  // runs the service one single time ([] notation) when component is first rendered
+  useEffect(() => {
+    startService();
+  }, []);
+
+  const onClick = () => {
+    if (!ref.current) {
+      return;
+    }
+    ref.current
+      .transform(input, {
+        loader: "jsx",
+        target: "es2015",
+      })
+      .then((result: any) => {
+        setCode(result.code);
+      });
+  };
+
+  return (
+    <div>
+      <textarea value={input} onChange={(e) => setInput(e.target.value)}>
+        {input}
+      </textarea>
+      <div>
+        <button onClick={onClick}>Submit</button>
+      </div>
+      <pre>{code}</pre>
+    </div>
+  );
 };
 
 ReactDOM.render(<App />, document.querySelector("#root"));
