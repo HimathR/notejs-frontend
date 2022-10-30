63cb529ec7c1d29e82b0eeb256835b1b9c5bbfcf
Himath_R <himath4510@gmail.com>
2022-10-30 21:19:47 +1000

    more refactoring on filestructure

--- src/components/preview.tsx
+++ src/components/preview.tsx
@@ -1,32 +1,31 @@
-import { useEffect, useRef } from "react";
+import { useRef, useEffect } from "react";
 
 interface PreviewProps {
   code: string;
 }
 
 const html = `
-<html>
-  <head></head>
-  <body>
-    <div id="root"></div>
-    <script>
-    window.addEventListener('message', (event) => {
-      try {
-        eval(event.data);
-      } catch (err) {
-        const root = document.querySelector('#root');
-        root.innerHTML = '<div style="color: red;"><h4>Runtime Error</h4>' + err + '</div>';
-        console.error(err);
-      }
-    }, false);
-    </script>
-  </body>
-</html>
-`;
+    <html>
+      <head></head>
+      <body>
+        <div id="root"></div>
+        <script>
+          window.addEventListener('message', (event) => {
+            try {
+              eval(event.data);
+            } catch (err) {
+              const root = document.querySelector('#root');
+              root.innerHTML = '<div style="color: red;"><h4>Runtime Error</h4>' + err + '</div>';
+              console.error(err);
+            }
+          }, false);
+        </script>
+      </body>
+    </html>
+  `;
 
 const Preview: React.FC<PreviewProps> = ({ code }) => {
   const iframe = useRef<any>();
-  iframe.current.srcdoc = html; // reset iframe contents
 
   useEffect(() => {
     iframe.current.srcdoc = html;
@@ -35,8 +34,8 @@ const Preview: React.FC<PreviewProps> = ({ code }) => {
 
   return (
     <iframe
+      title="preview"
       ref={iframe}
-      title="codeprev"
       sandbox="allow-scripts"
       srcDoc={html}
     />
